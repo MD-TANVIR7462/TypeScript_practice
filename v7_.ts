647ad3694c6100223a8fe74892b7ff638229d3dc
Tajul Islam <mdtanvir7462@gmail.com>
2024-07-07 15:49:28 +0600

    v7_function array declaration

--- v7_.ts
+++ v7_.ts
@@ -1,10 +1,24 @@
-function addHandler (num1:number,num2:number):number{
-    return num1+num2
+function addHandler(num1: number, num2: number): number {
+  return num1 + num2;
 }
-console.log(addHandler(2,2))
+console.log(addHandler(2, 2));
 
-const ArroHandelar = (num1:number,num2:number):number=>{
-    return num1+num2
-}
+const ArroHandelar = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
 
-console.log(ArroHandelar(2,2))
+console.log(ArroHandelar(2, 2));
+
+const poorUser = {
+  name: "poorUser",
+  balance: 0,
+  addBalance(balance: number): number {
+    return this.balance + balance;
+  },
+};
+
+
+const numberArray : number[] = [1,2,3,4,5,6,7,8,9]
+const arrayHandelar = numberArray.map((signleElem:number) :number=> signleElem+signleElem)
+
+
