70a3248b1edab2c95bf14f49c9bfd612eaf94d45
Tajul Islam <mdtanvir7462@gmail.com>
2024-07-30 15:30:16 +0600

    Typescript Generic Func

--- Module-2/generic.func.v5.ts
+++ Module-2/generic.func.v5.ts
@@ -1,8 +1,35 @@
 //function with generics........
 
-const creat = (x:string):string[]=>{
-    return [x]
+const genericFunc = <T>(argValue: T): T[] => {
+  return [argValue];
+};
+console.log(genericFunc<string>("hellow"));
+console.log(genericFunc<number>(12));
+console.log(
+  genericFunc<{
+    name: string;
+    roll: number;
+  }>({
+    name: "roja",
+    roll: 12,
+  })
+);
 
-}
+const genericFuncTuple = <T, y>(argValue: T, argValue2: y): [T, y] => {
+  return [argValue, argValue2];
+};
 
-console.log(creat("hello"))
+//...
+type TUser = {
+  name: string;
+  role: number;
+  devType: string;
+};
+type TUser2 = TUser & { PcType: string };
+
+const user: TUser & {PcType:string} = {
+  name: "user1",
+  role: 12,
+  devType: "MERN",
+  PcType: "MERN PC",
+};
